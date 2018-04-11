a886c185f8203caf58164d4eb6faa36d842ec28a
Adam Murray <adam@tikabu.com.au>
2018-04-11 17:17:18 +1000

    Updated tests to not rely on hard coded path

--- Tests/Public/New-Subnet.Tests.ps1
+++ Tests/Public/New-Subnet.Tests.ps1
@@ -1,18 +1,19 @@
-﻿$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace("Tests\Public","Summit2018\Public")
+﻿$moduleName = ($MyInvocation.MyCommand.Path -split '\\')[-4]
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace("Tests\Public","$moduleName\Public")
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 
 . "$here\$sut"
 
 # To make test runable from project root, and from test directory itself. Do quick validation.
 if ((Get-Location).Path -match "\\Tests\\Public") {
-    $psmPath = (Resolve-Path "..\..\Summit2018\Summit2018.psm1").Path    
+    $psmPath = (Resolve-Path "..\..\$moduleName\$moduleName.psm1").Path
 } else {
-    $psmPath = (Resolve-Path ".\Summit2018\Summit2018.psm1").Path
+    $psmPath = (Resolve-Path ".\$moduleName\$moduleName.psm1").Path
 }
 
 Import-Module $psmPath -Force -NoClobber
 
-InModuleScope "Summit2018" {
+InModuleScope "$moduleName" {
 
     Describe "New-Subnet" {
         It "Should throw if ip is not valid" {
--- Tests/Public/get-summit.Tests.ps1
+++ Tests/Public/get-summit.Tests.ps1
@@ -1,25 +1,26 @@
-﻿$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace("Tests\Public","Summit2018\Public")
+﻿$moduleName = ($MyInvocation.MyCommand.Path -split '\\')[-4]
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace("Tests\Public","$moduleName\Public")
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 
 . "$here\$sut"
 
 # To make test runable from project root, and from test directory itself. Do quick validation.
 if ((Get-Location).Path -match "\\Tests\\Public") {
-    $psmPath = (Resolve-Path "..\..\Summit2018\Summit2018.psm1").Path    
+    $psmPath = (Resolve-Path "..\..\$moduleName\$moduleName.psm1").Path
 } else {
-    $psmPath = (Resolve-Path ".\Summit2018\Summit2018.psm1").Path
+    $psmPath = (Resolve-Path ".\$moduleName\$moduleName.psm1").Path
 }
 
 Import-Module $psmPath -Force -NoClobber
 
-InModuleScope "Summit2018" {
+InModuleScope "$moduleName" {
     get-summit
 
     Describe "get-summit" {
         It 'Passing test' {
             1 | Should -Be 1
         }
-        
+
 
     }
 
